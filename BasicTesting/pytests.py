5b67e392524e6d612181c3aa78d0c27862d5ea9d
WainTrain <jkwainwright13@gmail.com>
2023-03-04 19:44:10 +1100

    Use proper formatting for empty test

--- BasicTesting/pytests.py
+++ BasicTesting/pytests.py
@@ -1,5 +1,5 @@
 # File containing general pytests. Keep in mind that Github CI will not come up green if any test is unsuccessful.
 
 # Empty test to allow CI to pass & ensure pytest is running correctly.
-def test_empty:
+def test_empty():
     pass
