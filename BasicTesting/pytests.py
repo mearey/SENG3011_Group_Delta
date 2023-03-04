798fe26ebaf746ab5697a78fb8f2b45bfc69055c
WainTrain <jkwainwright13@gmail.com>
2023-03-04 19:42:33 +1100

    Create pytests file with empty test for CI pass

---
+++ BasicTesting/pytests.py
@@ -0,0 +1,5 @@
+# File containing general pytests. Keep in mind that Github CI will not come up green if any test is unsuccessful.
+
+# Empty test to allow CI to pass & ensure pytest is running correctly.
+def test_empty:
+    pass
