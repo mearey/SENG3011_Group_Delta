23b515aa8e645a39a2f6b4c40a7129b095909d51
mearey <mearey67@gmail.com>
2023-03-08 12:35:59 +1100

    Webscraper for WHO website MVP finalised pytests still needed

--- BasicTesting/WebScraper.py
+++ BasicTesting/WebScraper.py
@@ -1,37 +1,54 @@
 import requests
 import json
+import nltk
+nltk.download('averaged_perceptron_tagger')
 from bs4 import BeautifulSoup
 
-WHOURL = "https://www.who.int/emergencies/disease-outbreak-news"
+def checkForMultiCountry(l):
+    for item in l:
+        if "Multi" in item:
+            return "Multi"
+        if "Global" in item:
+            return "Global"
+    return "None"
 
-i = 140
+def refreshAllJsonData(i):
+    f = open("WHOdataTest.json", "a")
+    dataFormatted = json.loads("[]")
 
-f = open("WHOdataTest.json", "a")
-dataFormatted = json.loads("[]")
-while i > 0:
-    WHOURL = "https://www.who.int/emergencies/disease-outbreak-news/" + str(i)
-    i = i - 1
+    while i > 0:
+        WHOURL = "https://www.who.int/emergencies/disease-outbreak-news/" + str(i)
+        i = i - 1
+        who = requests.get(WHOURL)
+        soup = BeautifulSoup(who.content, "html.parser")
+        content = soup.find(id="PageContent_C010_Col00")
+        list = content.find_all(class_="sf-list-vertical__item")
 
-    who = requests.get(WHOURL)
-    soup = BeautifulSoup(who.content, "html.parser")
-    content = soup.find(id="PageContent_C010_Col00")
-    list = content.find_all(class_="sf-list-vertical__item")
+        for item in list:
+            title = item.find(class_="sf-list-vertical__title") 
+            strings = title.find_all()
+            date = strings[1].text
+            locationAndDisease = strings[2].text.split(" - ")
 
-    for item in list:
-        title = item.find(class_="sf-list-vertical__title") 
-        strings = title.find_all()
+            disease = nltk.pos_tag(nltk.word_tokenize(locationAndDisease[0]))
+            if disease == "Multi":
+                disease = nltk.pos_tag(nltk.word_tokenize(locationAndDisease[1]))
 
-        date = strings[1].text
-        locationAndDisease = strings[2].text.split("-")
-        disease = locationAndDisease[0]
-        location = "N/A"
-        
-        if len(location) == 2:
-            location = locationAndDisease[1]
-        
-        jsonData = {"date":date, "disease":disease, "location" : location}
+            for item in disease:
+                if item[1] == "NNP" or "NN" or "NNS":
+                    disease = item[0]
+                    break
 
-        dataFormatted.append(jsonData)
-        
-f.write(json.dumps(dataFormatted))
-f.close()
+            location = "N/A"
+            multiCountry = checkForMultiCountry(locationAndDisease)
+            if multiCountry != "None":
+                location = multiCountry
+            elif len(locationAndDisease) >= 2:
+                location = locationAndDisease[len(locationAndDisease)-1]
+            
+            jsonData = {"date":date, "disease":disease, "location" : location}
+
+            dataFormatted.append(jsonData)
+            
+    f.write(json.dumps(dataFormatted))
+    f.close()
