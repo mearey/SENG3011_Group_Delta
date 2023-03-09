e22831ef845443868d6816327bbcd8ee3784ad8c
mearey <mearey67@gmail.com>
2023-03-09 13:04:48 +1100

    Epiwatch API Data script complete

--- BasicTesting/EpiwatchAPI.py
+++ BasicTesting/EpiwatchAPI.py
@@ -1,17 +1,22 @@
 import requests
+import json
 
 URL = "https://epiwatch-api.azurewebsites.net/api/grid"
-
 page = requests.post(URL)
-
 data = page.json()
+dataList = []
 
-def GetDataType(datatype):
-    l = []
-    if datatype == "all":
-        for item in data["results"]:
-            l.append(item)
-    else:
-        for item in data["results"]:
-            l.append(item[datatype])
-    return l
+def GetEpiwatchData():
+    for item in data["results"]:
+        print(item)
+        date = item["publication_date"]
+        disease = item["diseases"]
+        location = item["country_name"]
+        dataFormatted = {"date":date, "disease":disease, "location" : location}
+        dataList.append(dataFormatted)
+    return json.dumps(dataList)
+
+def WriteDataToFile(filename):
+    f = open(filename, "a")
+    f.write(GetEpiwatchData())
+    f.close()
